c283ca628b68af6eff505c14fcfec6c34be77cf5
codystanley <cody.stanley@triagelogic.com>
2024-04-28 02:00:26 -0400

    included slim framework

--- public/index.php
+++ public/index.php
@@ -1,7 +1,20 @@
 <?php
-require '../vendor/autoload.php';
-require '../config.php';
-include 'header.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+// require 'config.php';
+// include 'header.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
 ?>
         <section class="row m-3" id="loginSection">
             <div class="col-12" id="loginButtonDiv">
